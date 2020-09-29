312c6609a2971e4a16a2e7891f6b54e30fca4e7d
Dylan Jack <fall_back_up@hotmail.com>
2020-09-29 02:26:22 -0700

    Added UnitTests, remade Grid

---
+++ Assets/Scripts/Grid/GridController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridController : MonoBehaviour
+{
+
+    public Tilemap map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+        var mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3Int coordinate = map.WorldToCell(mouseWorldPos);
+        Debug.Log(coordinate);
+        Debug.Log(mouseWorldPos);
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var tile = map.GetTile(coordinate);
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/Conway.Tests/ConwayRulesTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ConwayRulesTests
+    {
+
+        //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        [Test]
+        public void LiveCell_LessThanTwoLiveNeighbours_Dies()
+        {
+            
+        }
+
+        //Any live cell with two or three live neighbours lives on to the next generation.
+        [Test]
+        public void LiveCell_TwoOrThreeLiveNeighbours_Lives()
+        {
+
+        }
+
+        //Any live cell with more than three live neighbours dies, as if by overpopulation.
+        [Test]
+        public void LiveCell_GreaterThanThreeLiveNeighbours_Dies()
+        {
+
+        }
+
+        //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        [Test]
+        public void DeadCell_ExactlyThreeLiveNeighbours_Lives()
+        {
+
+        }
+    }
+}
